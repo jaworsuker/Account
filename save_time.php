f0931748660e40817eb2c4bb5736d624b5b3fccd
jaworsuker <39408268+jaworsuker@users.noreply.github.com>
2018-11-06 14:25:05 +0100

    Add files via upload

---
+++ save_time.php
@@ -0,0 +1,14 @@
+<?php
+
+
+require 'conf.ini.php';
+require 'class/connectDB.php';
+
+
+/*head*/
+require 'partial/head.php'; 
+
+require 'partial/save.php';
+
+/*footer*/
+require 'partial/footer.php'; 
